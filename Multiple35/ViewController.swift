6aae14ce7072ec4912c9d0b0c36ada93767d0820
前田恵里 <eri@maedaeri-no-macbook-pro.local>
2016-09-01 00:46:12 +0900

    Initial Commit

---
+++ Multiple35/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multiple35
+//
+//  Created by 前田恵里 on 2016/09/01.
+//  Copyright © 2016年 前田恵里. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
